d1c25d8a68693cea9ce3f270ae2b9f91db811903
Casper Schuijt <casperschuijt@gmail.com>
2022-08-15 22:45:36 +0200

    Cycle Hero model data in one go

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,4 +1,21 @@
 class Hero < ApplicationRecord
   serialize :roles
-  
+
+  def self.refresh
+    # We're atomic baby
+    ActiveRecord::Base.transaction do
+      self.destroy_all
+
+      # Iterate over heroes OpenDota knows about and add them to our database
+      OpendotaHeroes.all.each do |hero|
+        h = self.new
+        hero.each_pair do |k, v|
+          k = "hero_id" if k == "id"
+          v = "Outworld Destroyer" if v == "Outworld Devourer"
+          h[k] = v
+        end
+        h.save
+      end
+    end
+  end
 end
