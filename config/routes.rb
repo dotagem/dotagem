4f853a30e2425c53e29523a09abf591ab05ec4cb
Casper Schuijt <casperschuijt@gmail.com>
2022-08-16 20:47:18 +0200

    WIP omniauth implementation

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   telegram_webhook TelegramWebhooksController
+  
+  get 'auth/:provider/callback', to: 'sessions#create'
 end
