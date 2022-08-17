ba1b9d8e0a293fd30ec310e698805d04ae3915d8
Casper Schuijt <casperschuijt@gmail.com>
2022-08-17 14:45:28 +0200

    Session helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+
+  private
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "You need to sign in with Telegram first!"
+        redirect_to '/' 
+      end
+    end
 end
