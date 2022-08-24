76839a75de74aaaf459124fc7a5c0ebfff996fb3
Casper Schuijt <casperschuijt@gmail.com>
2022-08-24 15:04:58 +0200

    Seeding aliases from file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Fetch constant data from OpenDota
+GameMode.refresh
+Hero.refresh
+Item.refresh
+LobbyType.refresh
+Patch.refresh
+Region.refresh
+
+# Parse custom aliases from file
+# Aliases should be presented as an array of hashes, each
+# hash containing a "name" and a "hero_id" field
+# if "hero_id" is not present, give the full name of a hero as "hero"
+# and we'll try and match it to the aliases currently on file
+aliases = JSON.parse(File.read("#{Rails.root}/db/hero_aliases.json"))
+aliases.each do |data|
+  a = Alias.new
+  a.hero_id   = data["hero_id"] || Alias.find_by(name: data["hero"]).hero.hero_id
+  a.name      = data["name"].downcase
+  a.from_seed = true
+  a.save
+end
