eb9754cfc56c746d687cb9b19048164b8d876e72
Casper Schuijt <casperschuijt@gmail.com>
2022-08-27 21:00:27 +0200

    Structure for /match

--- app/controllers/telegram_matches_controller.rb
+++ app/controllers/telegram_matches_controller.rb
@@ -7,8 +7,15 @@ class TelegramMatchesController < Telegram::Bot::UpdatesController
   include MessageSession
 
   def match!(*args)
-    if args.any?
-      
+    if args.any? && args.count == 1 && args.first.to_i > 0
+      match_id = args.first.to_i
+      @match = Match.from_api(match_id)
+
+      result = respond_with :message, text: build_match_overview_message(@match),
+        reply_markup: {inline_keyboard: build_match_mode_buttons("overview")}
+      message_session(result['result']['message_id'])
+      message_session[:match]      = @match
+      message_session[:match_mode] = "overview"
     else
       respond_with :message, text: "You need to specify a match ID! It may be" +
       " easier to use /matches to find the match you are looking for."
@@ -16,11 +23,11 @@ class TelegramMatchesController < Telegram::Bot::UpdatesController
   end
 
   def match_callback_query(match_id)
-    session[:match] = Match.from_api(match_id.to_i)
+    session[:match]      = Match.from_api(match_id.to_i)
     session[:match_mode] = "overview"
 
     edit_message :text, text: build_match_overview_message(session[:match])
-    edit_message :reply_markup, reply_markup: build_match_mode_buttons("overview")
+    edit_message :reply_markup, reply_markup: {inline_keyboard: build_match_mode_buttons("overview")}
 
     answer_callback_query ""
   end
@@ -28,6 +35,7 @@ class TelegramMatchesController < Telegram::Bot::UpdatesController
   def match_mode_callback_query(mode)
 
   end
+
 
   private
 
