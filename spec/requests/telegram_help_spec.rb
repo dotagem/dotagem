c67a04fd644d8c6ce351394a0493f2f69fc0aca3
Casper Schuijt <casperschuijt@gmail.com>
2022-08-30 00:14:01 +0200

    TelegramHelpController specs

--- spec/requests/telegram_help_spec.rb
+++ spec/requests/telegram_help_spec.rb
@@ -2,20 +2,24 @@ RSpec.describe TelegramHelpController, telegram_bot: :rails do
   describe "#help!" do
     it "should respond with help text" do
       expect {dispatch_message("/help")}
-      .to respond_with_message(//)
+      .to respond_with_message(/For a list of commands/)
     end
 
     it "should not care about arguments" do
       expect {dispatch_message("/help a b c")}
-      .to respond_with_message(//) 
+      .to respond_with_message(/For a list of commands/) 
     end
   end
 
-  describe "#commands!" do
-
-  end
-
   describe "#start!" do
+    it "should respond with help text" do
+      expect {dispatch_message("/start")}
+      .to respond_with_message(/Hello! I can fetch your Dota 2 match data/)
+    end
 
+    it "should not care about arguments" do
+      expect {dispatch_message("/start as df sfdlkwe")}
+      .to respond_with_message(/Hello! I can fetch your Dota 2 match data/)
+    end
   end
 end
