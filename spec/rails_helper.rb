fe96a9bcdbb97e4fa1878bdfe9e9206a250dd8da
Casper Schuijt <casperschuijt@gmail.com>
2022-08-30 11:50:46 +0200

    Seed the database before tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,6 +30,11 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  # Seed the database to ensure default data is present
+  config.before(:suite) do
+    Rails.application.load_seed
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
