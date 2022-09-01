ec8217bdee40c80b25731220a3a3baa9aedffb56
Casper Schuijt <casperschuijt@gmail.com>
2022-09-01 14:50:31 +0200

    Players spec structure

---
+++ spec/requests/telegram_players_spec.rb
@@ -0,0 +1,58 @@
+RSpec.describe TelegramPlayersController, telegram_bot: :rails do
+  describe "#matches!" do
+    context "without arguments" do
+      it "should return the most recent matches" do
+
+      end
+
+      it "should paginate more than 5 results" do
+
+      end
+
+      it "should not paginate less than 6 results" do
+
+      end
+    end
+
+    context "with clear arguments" do
+      it "should handle hero arguments" do
+
+      end
+
+      it "should handle teammate arguments" do
+
+      end
+
+      it "should be able to fetch matches for a different player" do
+
+      end
+
+      it "should return matches for those arguments" do
+
+      end
+
+      it "should not return other matches" do
+
+      end
+    end
+
+    context "with aliases" do
+      it "should ask for clarification" do
+
+      end
+
+      it "should pick the correct hero after button press" do
+
+      end
+
+      it "should ask multiple times if necessary" do
+
+      end
+
+      it "should return a list of matches once everything is clear" do
+
+      end
+    end
+  end
+  
+end
