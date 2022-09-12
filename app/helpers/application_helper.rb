c49d47852c371eb0469f3f40e1c3da440c5c010d
Casper Schuijt <casperschuijt@gmail.com>
2022-09-12 12:09:06 +0200

    Dynamic page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title_helper(page_title=nil)
+    base_title = "Gem of True Sight"
+
+    if page_title.present?
+      [page_title, base_title].join(" | ")
+    else
+      base_title
+    end
+  end
 end
