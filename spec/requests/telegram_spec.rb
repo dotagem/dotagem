2b282616c085d5eef3c6d182b9b053beea3afadb
Casper Schuijt <casperschuijt@gmail.com>
2022-09-16 18:51:37 +0200

    Specs for auto-updating user data

--- spec/requests/telegram_spec.rb
+++ spec/requests/telegram_spec.rb
@@ -1,4 +1,84 @@
 RSpec.describe "Telegram bot", telegram_bot: :rails do
+  describe "user data" do
+    let(:user) { create(:user, :steam_registered) }
+
+    it "should update user's username when they run a command" do
+      original_username = user.telegram_username
+      new_username = "abcdefg"
+
+      expect(user.telegram_username).to eq(original_username).and not_eq(new_username)
+
+      dispatch_message("/help", from: {
+        id: user.telegram_id,
+        username: new_username,
+        first_name: user.telegram_name
+      })
+
+      expect(user.reload.telegram_username).to eq(new_username).and not_eq(original_username)
+    end
+
+    it "should update user's name when they run a command" do
+      original_name = user.telegram_name
+      new_name = "Hello"
+
+      expect(user.telegram_name).to eq(original_name).and not_eq(new_name)
+
+      dispatch_message("/help", from: {
+        id: user.telegram_id,
+        username: user.telegram_username,
+        first_name: new_name
+      })
+
+      expect(user.reload.telegram_name).to eq(new_name).and not_eq(original_name)
+    end
+
+    it "should put first and last name together correctly" do
+      original_name = user.telegram_name
+
+      expect(user.telegram_name).to eq(original_name)
+
+      dispatch_message("/help", from: {
+        id: user.telegram_id,
+        username: user.telegram_username,
+        first_name: "Hello",
+        last_name: "World"
+      })
+
+      expect(user.reload.telegram_name).to eq("Hello World").and not_eq(original_name)
+    end
+
+    it "should update when user runs a callback query" do
+      dispatch(callback_query: {
+        data: "nothing:0", message: {message_id: 60, chat: {id: 456}},
+        from: {
+          id: user.telegram_id,
+          username: "hehexd",
+          first_name: "Lol Lmao"
+        }
+      })
+
+      expect(user.reload.telegram_username).to eq("hehexd")
+      expect(user.reload.telegram_name).to eq("Lol Lmao")
+    end
+
+    it "should update when user runs an inline query" do
+      dispatch(
+        inline_query: {
+          id:   "347",
+          from: {
+            id: user.telegram_id,
+            username: "hehexd",
+            first_name: "Lol Lmao"
+          },
+          query: "spirit"
+        }
+      )
+
+      expect(user.reload.telegram_username).to eq("hehexd")
+      expect(user.reload.telegram_name).to eq("Lol Lmao")
+    end
+  end
+
   it "should welcome new group members" do
     dispatch(message: {
       from: {id: 12345, username: "asdfgh"},
