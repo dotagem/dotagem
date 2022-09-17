efb487f2b5cc3a52ab9ebc54f2e82277e85210c5
Casper Schuijt <casperschuijt@gmail.com>
2022-09-17 14:10:35 +0200

    Rename Alias to Nickname

--- app/controllers/constants_controller.rb
+++ app/controllers/constants_controller.rb
@@ -9,7 +9,7 @@ class ConstantsController < ApplicationController
     Patch.refresh
     Region.refresh
 
-    Alias.destroy_by(from_seed: true)
+    Nickname.destroy_by(from_seed: true)
     # Parse custom aliases from file
     # Aliases should be presented as an array of hashes, each
     # hash containing a "name" and a "hero_id" field
@@ -18,8 +18,8 @@ class ConstantsController < ApplicationController
     aliases = JSON.parse(File.read("#{Rails.root}/db/hero_aliases.json"))
 
     aliases.each do |data|
-      a = Alias.new
-      a.hero_id   = data["hero_id"] || Alias.find_by(name: data["hero"]).hero.hero_id
+      a = Nickname.new
+      a.hero_id   = data["hero_id"] || Nickname.find_by(name: data["hero"]).hero.hero_id
       a.name      = data["name"].downcase
       a.from_seed = true
       a.save
