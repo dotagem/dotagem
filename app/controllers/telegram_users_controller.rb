00ade53fa0fe0ca6b9388ed078cde4f03c22f7fd
Casper Schuijt <casperschuijt@gmail.com>
2022-09-21 22:17:43 +0200

    Login after /login as well

--- app/controllers/telegram_users_controller.rb
+++ app/controllers/telegram_users_controller.rb
@@ -8,27 +8,56 @@ class TelegramUsersController < Telegram::Bot::UpdatesController
   # if you're looking for player data commands, check TelegramPlayersController
 
   def login!(*)
+    web_button = [
+      {
+        text: "Log In (website)",
+        login_url: {url: login_callback_url}
+      }
+    ]
+
+    message_button = [
+      {
+        text: "Log In (message)",
+        url: "https://t.me/#{bot.username}?start=login"
+      }
+    ]
+
     current_user = User.find_by(telegram_id: from["id"])
     if current_user && current_user.steam_registered?
       message = "Your registration is complete and you can now use the bot! " +
                 "If you want to unlink your account, use the button below to " +
                 "log in or go to #{Rails.application.credentials.base_url} " +
                 "and log in there."
+      respond_with :message,
+        text: message,
+        reply_markup: {
+          inline_keyboard: [web_button]
+        }
     else
-      message = "To use this bot, you need to log in with both Steam and " +
-                "Telegram. To complete your registration, use the button " +
-                "below to log in instantly, or go to " +
-                "#{Rails.application.credentials.base_url} and log in there."
+      if update["message"]["chat"]["type"] == "private"
+        save_context :login_from_message
+        message = "To use this bot, you need to log in with both Steam and " +
+                  "Telegram. To complete your registration, you have two options:" +
+                  "\n\na) Use the button below to log in with Steam via the website, or\n" +
+                  "b) Send me a message with a link to your Steam profile and " +
+                  "complete your registration that way."
+        respond_with :message,
+          text: message,
+          reply_markup: {
+          inline_keyboard: [web_button]
+        }
+      else
+        message = "To use this bot, you need to log in with both Steam and " +
+        "Telegram. To complete your registration, open a PM with " +
+        "me using the button below or go to the website and log in " +
+        "with Steam there."
+        respond_with :message,
+          text: message,
+          reply_markup: {
+            inline_keyboard: [web_button, message_button]
+          }
+      end
     end
-    respond_with :message,  text: message,
-                            reply_markup: {
-                              inline_keyboard: [
-                                [{
-                                  text: "Log In",
-                                  login_url: {url: login_callback_url}
-                                }]
-                              ]
-                            }
   end
 
   alias_method :log_in!,  :login!
@@ -41,13 +70,13 @@ class TelegramUsersController < Telegram::Bot::UpdatesController
               "below or go to #{Rails.application.credentials.base_url} " +
               "and log in."
     respond_with  :message, text: message,
-                            reply_markup: {
-                              inline_keyboard: [
-                                [{
-                                  text: "Log In",
-                                  login_url: {url: login_callback_url}
-                                }]
-                              ]
-                            }
+      reply_markup: {
+        inline_keyboard: [
+          [{
+            text: "Log In",
+            login_url: {url: login_callback_url}
+          }]
+        ]
+      }
   end
 end
